42a6f58e8fd6ef9d9472866754080f5d4b2c9f49
Chris Paterson <chrispaterson22@gmail.com>
2021-01-15 17:04:21 +0000

    complete react todo app from travesty media tutorial completed by myself

---
+++ src/components/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <p>This is the TodoList app v1.0.0. It is part of a React course from
+        Travesty Media</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
